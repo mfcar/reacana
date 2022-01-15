4cc2cc5166daf268d15f365e910c3644fbdd7b71
Marcos Carvalho <marcosfcar@gmail.com>
2022-01-15 15:16:36 -0300

    Prettier

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Header from './Header';
 import ShowCharacters from './ShowCharacters';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <ShowCharacters />
-    </div>
-  );
+   return (
+      <div className="App">
+         <Header />
+         <ShowCharacters />
+      </div>
+   );
 }
 
 export default App;
--- src/ShowCharacters.js
+++ src/ShowCharacters.js
@@ -1,15 +1,15 @@
 import katakana from './data/katakanaSymbols.json';
 
 function ShowCharacters() {
-  return (
-    <ul>
-      {katakana.map((kata) => (
-        <li key={kata.transliteration}>
-          {kata.katakana} - {kata.transliteration}
-        </li>
-      ))}
-    </ul>
-  );
+   return (
+      <ul>
+         {katakana.map((kata) => (
+            <li key={kata.transliteration}>
+               {kata.katakana} - {kata.transliteration}
+            </li>
+         ))}
+      </ul>
+   );
 }
 
 export default ShowCharacters;
